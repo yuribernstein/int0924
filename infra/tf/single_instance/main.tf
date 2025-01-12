c569cf2af969e939dd31b9fbb327a8243c2c0c5a
Bernstein <elena.bernstein2@gmail.com>
2025-01-12 10:18:03 -0700

    more tf examples

--- infra/tf/single_instance/main.tf
+++ infra/tf/single_instance/main.tf
@@ -11,9 +11,40 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
+
+variable "Purpose" {
+  default = "Development"
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-05d38da78ce859165"
+  instance_type = "t2.micro"
+  count        = 2
+    tags = {
+    Purpose     = var.Purpose  
+    Name        = "Backend"
+    Owner       = "Yuri"
+  }
+}
+
+
+resource "aws_instance" "frontend" {
+  ami           = "ami-05d38da78ce859165"
+  instance_type = "t2.micro"
+  count        = 2
+  tags = {
+    Name        = "Frontend"
+    Owner       = "Yuri"
+  }
+}
+
+
+resource "aws_instance" "proxy" {
   ami           = "ami-05d38da78ce859165"
   instance_type = "t2.micro"
   count        = 1
+  tags = {
+    Name        = "Proxy"
+    Owner       = "Yuri"
+  }
 }
-
